2bb6e197e78768b3b368ece76ece31e46b429158
nastiakh1301 <78373469+nastiakh1301@users.noreply.github.com>
2021-02-27 22:05:44 +0200

    Update task2.py

--- task2.py
+++ task2.py
@@ -45,14 +45,3 @@ assert is_lucky(479974) is True
 
 print("All tests passed successfully!")
 
-    
-assert is_lucky(1230) is True
-assert is_lucky(239017) is False
-assert is_lucky(134008) is True
-assert is_lucky(15) is False
-assert is_lucky(2020) is True
-assert is_lucky(199999) is False
-assert is_lucky(77) is True
-assert is_lucky(479974) is True
-
-print("All tests passed successfully!")
