4c39e81850ef5a1d76457b3e0848215368ae57ad
nastiakh1301 <78373469+nastiakh1301@users.noreply.github.com>
2021-02-27 22:04:34 +0200

    Update task2.py

--- task2.py
+++ task2.py
@@ -5,14 +5,15 @@
 import random
 
 def main():
-    ticket_num = str(random.randint(1, 100000000))
+    ticket_num = random.randint(0, 10000)
     is_lucky(ticket_num)
 
 def is_lucky(ticket_num):
     print(ticket_num)
+    ticket_num = str(ticket_num)
     first = 0
     second = 0
-    ticket = len(str(ticket_num))
+    ticket = len(ticket_num)
     if ticket % 2 == 0:
         for i in ticket_num[:int(ticket / 2)]:
             first += int(i)
@@ -20,19 +21,29 @@ def is_lucky(ticket_num):
             second += int(i)
         if first == second:
             print("Your ticket is lucky")   
-            return ticket_num
+            return True
         else:
             print("Your ticket is unlucky")
-            return ticket_num
+            return False
     else:
         print("Your ticket is unlucky")
-        return ticket_num
-
-
+        return False
 
 
 if __name__ == "__main__":
     main()
+
+
+assert is_lucky(1230) is True
+assert is_lucky(239017) is False
+assert is_lucky(134008) is True
+assert is_lucky(15) is False
+assert is_lucky(2020) is True
+assert is_lucky(199999) is False
+assert is_lucky(77) is True
+assert is_lucky(479974) is True
+
+print("All tests passed successfully!")
 
     
 assert is_lucky(1230) is True
